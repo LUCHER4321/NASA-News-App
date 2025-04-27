fe3d7cfaa7fcf4a73fb65ec114599a9532f5bac4
LUCHER4321 <lucher4321@gmail.com>
2025-04-26 22:11:09 -0400

    Create Coroutine.kt

---
+++ app/src/main/java/com/example/nasanewsapp/util/Coroutine.kt
@@ -0,0 +1,14 @@
+package com.example.nasanewsapp.util
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+
+fun coroutine(function: suspend () -> Unit){
+    val coroutineScope = CoroutineScope(Dispatchers.Default)
+    coroutineScope.launch {
+        function()
+        coroutineScope.cancel()
+    }
+}
